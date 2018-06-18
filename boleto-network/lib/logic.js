48741e5af16b8b63bba083fef5cec08e207dc14f
Arthur Souza <arthur.souza@cip-bancos.org.br>
2018-06-18 16:07:22 -0300

    atualizacao

--- boleto-network/lib/logic.js
+++ boleto-network/lib/logic.js
@@ -4,48 +4,47 @@
  * @transaction
  */
 function registrarBoleto(registroBoleto) {
-    var boleto = registroBoleto.boleto;
-    
-    if (boleto.boletoId == '') {
-      throw new Error('Não existe Boleto');
-    }
-   
-    boleto.codigoBarra= (Math.random() * Date.now()*10000000).toString()+(Math.random() * Date.now()*10000000).toString()+"000000"; 
-    var dias = 3;
-    boleto.dataVencimento = new Date(Date.now() + dias*24*60*60*1000).toLocaleString();
-    boleto.status = "PEDENTE";
-    
-    boleto.ifBeneficiario = registroBoleto.ifBeneficiario;
-    boleto.pagadorPF = registroBoleto.pagadorPF;
+  var boleto = registroBoleto.boleto;
   
-    console.log('###Registrar Boleto ID ' + boleto.boletoId.toString());
-    
-    return getAssetRegistry('org.boleto.Boleto')
-      .then(function(boletoRegistry) {
-      return boletoRegistry.update(boleto);
-    });
+  if (boleto.boletoId == '') {
+    throw new Error('Não existe Boleto');
+  }
+ 
+  boleto.codigoBarra= (Math.random() * Date.now()*10000000).toString()+(Math.random() * Date.now()*10000000).toString()+"000000"; 
+  var dias = 3;
+  boleto.dataVencimento = new Date(Date.now() + dias*24*60*60*1000).toLocaleString();
+  boleto.status = "PEDENTE";
+  
+  boleto.ifBeneficiario = registroBoleto.ifBeneficiario;
+  boleto.pagadorPF = registroBoleto.pagadorPF;
+
+  console.log('###Registrar Boleto ID ' + boleto.boletoId.toString());
+  
+  return getAssetRegistry('org.boleto.Boleto')
+    .then(function(boletoRegistry) {
+    return boletoRegistry.update(boleto);
+  });
+}
+
+/**
+ * Pagametno de boletos nos bancos
+ * @param {org.boleto.PagamentoBoleto} pagamentoBoleto - Transação com o Boleto que será pago por bancoPagador/IFPagadora
+ * @transaction
+ */
+
+async function pagarBoletoPorCodigoBarra(PagamentoBoletoCodigoBarra) {
+ 
+  const [ boleto ] =  await query('selectBoleto',{ 'codigoBarra': pagamentoBoleto.codigoBarra}); 
+  
+  if (boleto.boletoId == '') {
+    throw new Error('Não existe Boleto');
   }
   
-  /**
-   * Pagametno de boletos nos bancos
-   * @param {org.boleto.PagamentoBoleto} pagamentoBoleto - Transação com o Boleto que será pago por bancoPagador/IFPagadora
-   * @transaction
-   */
+  boleto.status = 'PAGO';
   
-  function pagarBoleto(pagamentoBoleto) {
-    var boleto = pagamentoBoleto.boleto;
-    console.log('###pagar Boleto ID ' + boleto.boletoId.toString());
-    
-    if (boleto.boletoId == '') {
-      throw new Error('Não existe Boleto');
-    }
-    
-    boleto.ifPagadora = pagamentoBoleto.ifPagadora;
-    boleto.status = 'PAGO';
-    
-    return getAssetRegistry('org.boleto.Boleto')
-      .then(function(boletoRegistry) {
-      return boletoRegistry.update(boleto);
-    });
-  }
   
+  return getAssetRegistry('org.boleto.Boleto')
+    .then(function(boletoRegistry) {
+    return boletoRegistry.update(boleto);
+  });
+}
