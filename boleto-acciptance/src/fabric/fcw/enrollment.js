77715b094b5fcd79061756b0d8b2080a2ed6cf2e
Arthur <arthur.msouza@gmail.com>
2020-05-05 13:46:42 -0300

    add files

--- boleto-acciptance/src/fabric/fcw/enrollment.js
+++ boleto-acciptance/src/fabric/fcw/enrollment.js
@@ -7,7 +7,7 @@ module.exports = function(logger) {
     const { FileSystemWallet, Gateway } = require('fabric-network');
     const fs = require('fs');
     var path = require('path');
-    const ccpPath = path.resolve(__dirname, '..', '..', 'basic-network', 'connection.json');
+    const ccpPath = path.resolve(__dirname, '..', '..', '..', '..', '..','basic-network', 'connection.json');
     const ccpJSON = fs.readFileSync(ccpPath, 'utf8');
     const ccp = JSON.parse(ccpJSON);
 
